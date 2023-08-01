2e3a6267e7bf9b0a2b0c88e3dfbd7a39c81c48ea
Ben Thornton <smenjas@users.noreply.github.com>
2023-07-31 21:40:22 -0700

    Allow valid rook moves.

--- client/square.js
+++ client/square.js
@@ -12,16 +12,8 @@ export default class Square {
     }
 
     static fileDown(file) {
-        switch (file) {
-            case 'b': return 'a';
-            case 'c': return 'b';
-            case 'd': return 'c';
-            case 'e': return 'd';
-            case 'f': return 'e';
-            case 'g': return 'f';
-            case 'h': return 'g';
-            default: return '';
-        }
+        const fileNumber = Square.fileToNumber(file);
+        return Square.numberToFile(fileNumber - 1);
     }
 
     static fileLeft(color, file) {
@@ -33,15 +25,35 @@ export default class Square {
     }
 
     static fileUp(file) {
+        const fileNumber = Square.fileToNumber(file);
+        return Square.numberToFile(fileNumber + 1);
+    }
+
+    static fileToNumber(file) {
         switch (file) {
-            case 'a': return 'b';
-            case 'b': return 'c';
-            case 'c': return 'd';
-            case 'd': return 'e';
-            case 'e': return 'f';
-            case 'f': return 'g';
-            case 'g': return 'h';
-            default: return '';
+        case 'a': return 1;
+        case 'b': return 2;
+        case 'c': return 3;
+        case 'd': return 4;
+        case 'e': return 5;
+        case 'f': return 6;
+        case 'g': return 7;
+        case 'h': return 8;
+        default: return 0;
+        }
+    }
+
+    static numberToFile(fileNumber) {
+        switch (fileNumber) {
+        case 1: return 'a';
+        case 2: return 'b';
+        case 3: return 'c';
+        case 4: return 'd';
+        case 5: return 'e';
+        case 6: return 'f';
+        case 7: return 'g';
+        case 8: return 'h';
+        default: return '';
         }
     }
 
