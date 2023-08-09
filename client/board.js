smenjas/chess-explorer

commit 95ec374d9f08d85edc3745cf399bdd28ba3b9431

Optimize findPawnJumps().
- Avoid the conditionals in Square.fileLeft() and Square.fileDown().
- Test for empty string equality instead of truthiness.

This is slightly faster.

client/board.js
@@ -363,13 +363,13 @@ export default class Board {
         }
         const moves = [];
         const r = (color === 'White') ? rank + 1 : rank - 1;
-        const left = Square.fileLeft(color, file);
-        const right = Square.fileRight(color, file);
-        if (left) {
-            this.addJump(moves, left + r, color, hypothetical);
+        const fileDown = Square.fileDown(color, file);
+        const fileUp = Square.fileUp(color, file);
+        if (fileDown !== '') {
+            this.addJump(moves, fileDown + r, color, hypothetical);
         }
-        if (right) {
-            this.addJump(moves, right + r, color, hypothetical);
+        if (fileUp !== '') {
+            this.addJump(moves, fileUp + r, color, hypothetical);
         }
         return moves;
     }

commit 188e4016d2ba02157fd7a9400a5766e6c5203afb

Fix findPawnJumps().
Fix a bug I introduced in: 95ec374d9f08d85edc3745cf399bdd28ba3b9431

client/board.js
@@ -363,8 +363,8 @@ export default class Board {
         }
         const moves = [];
         const r = (color === 'White') ? rank + 1 : rank - 1;
-        const fileDown = Square.fileDown(color, file);
-        const fileUp = Square.fileUp(color, file);
+        const fileDown = Square.fileDown(file);
+        const fileUp = Square.fileUp(file);
         if (fileDown !== '') {
             this.addJump(moves, fileDown + r, color, hypothetical);
         }