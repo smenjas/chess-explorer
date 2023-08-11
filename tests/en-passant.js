0ba1094d487dc913a87b66ccae8f0461dcf47032
Ben Thornton <smenjas@users.noreply.github.com>
2023-08-11 16:07:46 -0700

    Notate en passant tests.

--- tests/en-passant.js
+++ tests/en-passant.js
@@ -2,12 +2,13 @@ import Board from '../board.js';
 
 const tests = {};
 
+// See: https://en.wikipedia.org/wiki/En_passant
 tests['A white pawn can capture a black pawn en passant.'] = () => {
     const failures = [];
     const board = new Board();
     const moves = [
-        ['e2', 'e4'], ['c7', 'c6'],
-        ['e4', 'e5'], ['d7', 'd5'],
+        ['e2', 'e4'], ['c7', 'c6'], // 1. e4 c6
+        ['e4', 'e5'], ['d7', 'd5'], // 2. e5 d5
     ];
     if (board.testMoves(moves) === false) {
         failures.push('Move failed');
@@ -17,7 +18,7 @@ tests['A white pawn can capture a black pawn en passant.'] = () => {
         failures.push('En passant not recognized');
         return failures;
     }
-    if (board.testMoves([['e5', 'd6']]) === false) {
+    if (board.testMoves([['e5', 'd6']]) === false) { // 3. exd6
         failures.push('Move failed');
     }
     else if (board.squares['d5'] !== '') {
@@ -30,9 +31,9 @@ tests['A black pawn can capture a white pawn en passant.'] = () => {
     const failures = [];
     const board = new Board();
     const moves = [
-        ['e2', 'e4'], ['e7', 'e5'],
-        ['d2', 'd4'], ['e5', 'd4'],
-        ['c2', 'c4'],
+        ['e2', 'e4'], ['e7', 'e5'], // 1. e4 e5
+        ['d2', 'd4'], ['e5', 'd4'], // 2. d4? exd4
+        ['c2', 'c4'], // 3. c4
     ];
     if (board.testMoves(moves) === false) {
         failures.push('Move failed');
@@ -42,7 +43,7 @@ tests['A black pawn can capture a white pawn en passant.'] = () => {
         failures.push('En passant not recognized');
         return failures;
     }
-    if (board.testMoves([['d4', 'c3']]) === false) {
+    if (board.testMoves([['d4', 'c3']]) === false) { // 3... dxc3
         failures.push('Move failed');
     }
     else if (board.squares['c4'] !== '') {
