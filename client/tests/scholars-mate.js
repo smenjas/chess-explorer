d52d0487a88741c3100c1f16e780620595215c2d
Ben Thornton <smenjas@users.noreply.github.com>
2023-08-11 16:00:56 -0700

    Notate scholar's mate test.

--- client/tests/scholars-mate.js
+++ client/tests/scholars-mate.js
@@ -2,14 +2,15 @@ import Board from '../board.js';
 
 const tests = {};
 
+// See: https://en.wikipedia.org/wiki/Scholar%27s_mate
 tests['Scholar\'s mate works.'] = () => {
     const failures = [];
     const board = new Board();
     const moves = [
-        ['e2', 'e4'], ['e7', 'e5'],
-        ['d1', 'h5'], ['b8', 'c6'],
-        ['f1', 'c4'], ['g8', 'f6'],
-        ['h5', 'f7'],
+        ['e2', 'e4'], ['e7', 'e5'], // 1. e4 e5
+        ['d1', 'h5'], ['b8', 'c6'], // 2. Qh5 Nc6
+        ['f1', 'c4'], ['g8', 'f6'], // 3. Bc4 Nf6??
+        ['h5', 'f7'], // Qxf7# 1-0
     ];
     if (board.testMoves(moves) === false) {
         failures.push('Move failed');
