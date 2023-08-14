7b1362337f30a69e0e39e84fa3d4abad721ee2e7
Ben Thornton <smenjas@users.noreply.github.com>
2023-08-14 11:27:35 -0700

    Test Square.fileToNumber().

--- tests/square.js
+++ tests/square.js
@@ -3,6 +3,24 @@ import Test from '../test.js';
 
 const tests = {};
 
+tests['Square.fileToNumber() works.'] = () => {
+    const tests = [
+        [['a'], 1],
+        [['b'], 2],
+        [['c'], 3],
+        [['d'], 4],
+        [['e'], 5],
+        [['f'], 6],
+        [['g'], 7],
+        [['h'], 8],
+        [['H'], 0],
+        [['i'], 0],
+        [[''], 0],
+        [[1], 0],
+    ];
+    return Test.run(Square.fileToNumber, tests);
+};
+
 tests['Square.parse() works.'] = () => {
     const squares = {
         a1: ['a', 1],
