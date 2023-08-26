smenjas/chess-explorer

commit 79fb06020741a79927bb7be749215a2d4e72b85f

Implement an artificial opponent that moves randomly.

client/index.js
@@ -1,10 +1,29 @@
 import Board from './board.js';
 import Piece from './piece.js';
 
-function addEventHandlers() {
-    const squares = document.querySelectorAll('.chess-board td.can-move');
-    for (const square of squares) {
-        square.addEventListener('click', handleSelection);
+function addEventHandlers(board) {
+    if (board.players[board.turn] !== 'robot') {
+        const squares = document.querySelectorAll('.chess-board td.can-move');
+        for (const square of squares) {
+            square.addEventListener('click', handleSelection);
+        }
+    }
+
+    function handlePlayer(event) {
+        const menu = event.target;
+        const color = menu.id;
+        const player = menu[menu.selectedIndex].value;
+        board.players[color] = player;
+        updatePage(board);
+    }
+
+    const blackPlayer = document.getElementById('Black');
+    if (blackPlayer) {
+        blackPlayer.addEventListener('change', handlePlayer);
+    }
+    const whitePlayer = document.getElementById('White');
+    if (whitePlayer) {
+        whitePlayer.addEventListener('change', handlePlayer);
     }
 }
 
@@ -27,24 +46,61 @@ function renderPage(board) {
     let html = '<div id="container">';
     html += '<main id="board"></main>';
     html += '<div id="not-board">';
-    html += '<section id="buttons">';
-    html += '<button type="button" id="new-game">New Game</button>';
-    html += '</section>';
-    html += '<h1 id="tempo"></h1>';
-    html += '<aside id="score"></aside>';
+    html += renderUI(board);
     html += '</div>';
     html += '</div>';
     document.body.insertAdjacentHTML('beforeend', html);
     const newGameButton = document.getElementById('new-game');
     if (newGameButton) {
-        newGameButton.addEventListener('click', handleNewGame);
+        newGameButton.addEventListener('click', () => {
+            board = new Board(Board.fresh);
+            for (const color of ['Black', 'White']) {
+                document.getElementById(color).value = board.players[color];
+            }
+            updatePage(board);
+        });
     }
     updatePage(board);
 }
 
-function handleNewGame() {
-    board = new Board(Board.fresh);
-    updatePage(board);
+function renderOptions(options, selected) {
+    let html = '';
+    for (const option in options) {
+        const selectedAttr = (option === selected) ? ' selected' : '';
+        html += `<option value="${option}"${selectedAttr}>${options[option]}</option>`;
+    }
+    return html;
+}
+
+function renderSelect(id, options, selected) {
+    let html = `<select id="${id}">`;
+    html += renderOptions(options, selected);
+    html += '</select>';
+    return html;
+}
+
+function renderUI(board) {
+    const players = {
+        human: 'Human',
+        robot: 'Robot',
+    };
+    let html = '';
+    html += '<section id="ui">';
+    html += '<form>';
+    html += '<fieldset>';
+    html += '<div><button type="button" id="new-game">New Game</button></div>';
+    html += '</fieldset>';
+    html += '<fieldset>';
+    html += '<div class="player menu">Black: ' + renderSelect('Black', players, board.players.Black) + '</div>';
+    html += '<div class="player menu">White: ' + renderSelect('White', players, board.players.White) + '</div>';
+    html += '</fieldset>';
+    html += '</form>';
+    html += '</section>';
+    if (board.score.length) {
+        html += '<h1 id="tempo"></h1>';
+        html += '<aside id="score"></aside>';
+    }
+    return html;
 }
 
 function handleSelection(event) {
@@ -87,11 +143,25 @@ function selectPiece(square) {
     }
 }
 
-function updatePage(board) {
+async function updatePage(board) {
     document.getElementById('board').innerHTML = board.render();
-    document.getElementById('tempo').innerHTML = board.describe();
-    document.getElementById('score').innerHTML = board.renderScore();
-    addEventHandlers();
+    const tempo = document.getElementById('tempo');
+    if (tempo) {
+        tempo.innerHTML = board.describe();
+    }
+    const score = document.getElementById('score');
+    if (score) {
+        score.innerHTML = board.renderScore();
+    }
+    if (board.players[board.turn] === 'robot') {
+        await new Promise(resolve => setTimeout(resolve, 1));
+        const refresh = board.play();
+        if (refresh === true) {
+            updatePage(board);
+        }
+        return;
+    }
+    addEventHandlers(board);
 }
 
 let board = new Board();

client/board.js
@@ -27,6 +27,7 @@ export default class Board {
         drawCount: 0,
         score: [],
         history: [],
+        players: {Black: 'human', White: 'human'},
     };
     static ranks = [1, 2, 3, 4, 5, 6, 7, 8];
     static files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
@@ -38,6 +39,11 @@ export default class Board {
         for (const key in board) {
             this[key] = structuredClone(board[key]);
         }
+        for (const key in Board.fresh) {
+            if ((key in board) === false) {
+                this[key] = Board.fresh[key];
+            }
+        }
         if (hypothetical === true) {
             return;
         }
@@ -46,14 +52,14 @@ export default class Board {
 
     describe() {
         if (this.mate) {
-            return `Checkmate: ${this.getOpponent().toLowerCase()} wins`;
+            return `Checkmate<br>${this.getOpponent()} wins`;
         }
         if (this.draw !== '') {
             return `Draw due to ${this.draw}`;
         }
         let html = `${this.turn}'s move`;
         if (this.check) {
-            html += ', check';
+            html += '<br>Check';
         }
         return html;
     }
@@ -681,6 +687,34 @@ export default class Board {
         this.countRepetitions();
         this.detectDeadPosition();
         this.updateDrawCount(this.squares[to], captured);
+        return true;
+    }
+
+    chooseRandomMove() {
+        // What legal moves are there to choose from?
+        const choices = Object.keys(this.origins)
+            .filter(origin => this.origins[origin].length !== 0);
+        // Choose a piece randomly.
+        const fromIndex = Math.floor(Math.random() * choices.length);
+        const from = choices[fromIndex];
+        // Choose where to move randomly.
+        const moves = this.origins[from];
+        const toIndex = Math.floor(Math.random() * moves.length);
+        const to = moves[toIndex];
+        return [from, to];
+    }
+
+    chooseMove() {
+        return this.chooseRandomMove();
+    }
+
+    play() {
+        if (this.mate === true || this.draw !== '') {
+            return false;
+        }
+        const move = this.chooseMove();
+        this.move(...move);
+        this.save();
         return true;
     }
 

client/main.css
@@ -20,7 +20,9 @@ a:visited {
 
 h1 {
     font-size: 1.75rem;
-    margin: 0.75rem;
+    margin: 0.75rem 0;
+    width: 20rem;
+    height: 4.5rem;
     text-align: center;
 }
 
@@ -34,7 +36,7 @@ h1 {
 }
 
 #score {
-    height: calc(100vh - 6.75rem);
+    height: calc(100vh - 11.25rem);
     margin: 0;
     overflow: auto;
 }
@@ -43,12 +45,34 @@ h1 {
     margin: 0;
 }
 
-#buttons {
+#ui {
+    font-size: 1.5rem;
     text-align: center;
 }
 
-button#new-game {
+form {
+    display: flex;
+    margin: 0;
+    padding: 0;
+}
+
+fieldset {
+    margin: 0.1rem;
+    padding: 0;
+    width: 50%;
+    border: none;
+}
+
+button, select {
     font-size: 1.5rem;
+}
+
+.menu {
+    white-space: nowrap;
+}
+
+.player.menu {
+    text-align: right;
 }
 
 table {

commit 266e89287592c431f04b9464357d566a878bd6d1

Wrap event handlers for moves in a closure.
79fb06020741a79927bb7be749215a2d4e72b85f introduced a scope bug where there
were two instances of the Board object. The "New Game" button stopped working,
because handleMove referenced the older instance. This manifested as the board
reverting to a previous game upon moving a piece.

client/index.js
@@ -5,6 +5,61 @@ function addEventHandlers(board) {
     if (board.players[board.turn] === 'robot') {
         return;
     }
+
+    function addMoves(squares) {
+        for (const square of squares) {
+            square.classList.add('possible');
+            square.addEventListener('click', handleMove);
+        }
+    }
+
+    function cancelMoves(squares) {
+        for (const square of squares) {
+            square.classList.remove('possible');
+            square.removeEventListener('click', handleMove);
+        }
+    }
+
+    function handleMove(event) {
+        const square = event.target;
+        const squares = document.querySelectorAll('.chess-board td.selected');
+        const selected = squares[0];
+        board.move(selected.id, square.id);
+        board.save();
+        selected.classList.remove('selected');
+        highlightMoves(selected.id, true);
+        updatePage(board);
+    }
+
+    function handleSelection(event) {
+        const square = event.target;
+        for (const className of event.target.classList) {
+            if (Piece.exists(className)) {
+                selectPiece(square);
+                break;
+            }
+        }
+    }
+
+    function highlightMoves(square, cancel = false) {
+        const squares = document.querySelectorAll(`.chess-board td.from-${square}`);
+        const toggleMoves = cancel ? cancelMoves : addMoves;
+        toggleMoves(squares);
+    }
+
+    function selectPiece(square) {
+        const alreadySelected = square.classList.contains('selected');
+        const squares = document.querySelectorAll('.chess-board td.selected');
+        for (const s of squares) {
+            s.classList.remove('selected');
+            highlightMoves(s.id, true);
+        }
+        if (!alreadySelected) {
+            square.classList.add('selected');
+            highlightMoves(square.id);
+        }
+    }
+
     const squares = document.querySelectorAll('.chess-board td.can-move');
     for (const square of squares) {
         square.addEventListener('click', handleSelection);
@@ -39,20 +94,6 @@ function addFormHandlers(board) {
 
     document.getElementById('Black').addEventListener('change', handlePlayer);
     document.getElementById('White').addEventListener('change', handlePlayer);
-}
-
-function addMoves(squares) {
-    for (const square of squares) {
-        square.classList.add('possible');
-        square.addEventListener('click', handleMove);
-    }
-}
-
-function cancelMoves(squares) {
-    for (const square of squares) {
-        square.classList.remove('possible');
-        square.removeEventListener('click', handleMove);
-    }
 }
 
 function renderPage(board) {
@@ -112,46 +153,6 @@ function renderUI(board) {
         html += '<aside id="score"></aside>';
     }
     return html;
-}
-
-function handleSelection(event) {
-    const square = event.target;
-    for (const className of event.target.classList) {
-        if (Piece.exists(className)) {
-            selectPiece(square);
-            break;
-        }
-    }
-}
-
-function handleMove(event) {
-    const square = event.target;
-    const squares = document.querySelectorAll('.chess-board td.selected');
-    const selected = squares[0];
-    board.move(selected.id, square.id);
-    board.save();
-    selected.classList.remove('selected');
-    highlightMoves(selected.id, true);
-    updatePage(board);
-}
-
-function highlightMoves(square, cancel = false) {
-    const squares = document.querySelectorAll(`.chess-board td.from-${square}`);
-    const toggleMoves = cancel ? cancelMoves : addMoves;
-    toggleMoves(squares);
-}
-
-function selectPiece(square) {
-    const alreadySelected = square.classList.contains('selected');
-    const squares = document.querySelectorAll('.chess-board td.selected');
-    for (const s of squares) {
-        s.classList.remove('selected');
-        highlightMoves(s.id, true);
-    }
-    if (!alreadySelected) {
-        square.classList.add('selected');
-        highlightMoves(square.id);
-    }
 }
 
 function toggleLevel(players) {