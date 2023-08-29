ea05c045eb5bb2370b272f18db6a27fdbe9bf39e
Ben Thornton <smenjas@users.noreply.github.com>
2023-08-28 22:13:33 -0700

    Optimize chooseRandomly().

--- client/board.js
+++ client/board.js
@@ -772,6 +772,9 @@ export default class Board {
     }
 
     static chooseRandomly(array) {
+        if (array.length === 1) {
+            return array[0];
+        }
         const index = Math.floor(Math.random() * array.length);
         return array[index];
     }
