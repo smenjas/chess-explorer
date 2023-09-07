7b347d42cc84df30957e686b503d5ea7a7fabfdc
Ben Thornton <smenjas@users.noreply.github.com>
2023-09-07 12:36:23 -0700

    Log JSON size on the command line.
    I want to see this info without cluttering the browser console. I know this
    seems silly, since we don't save game state on the server.

--- play.js
+++ play.js
@@ -9,3 +9,8 @@ while (board.play()) {
 }
 
 console.log(board.describe().replace('<br>', ': '));
+
+// This is how much localStorage would be used in a browser.
+const json = JSON.stringify(board);
+const bytes = Buffer.byteLength(json);
+console.log('Board object size:', bytes, 'bytes');
